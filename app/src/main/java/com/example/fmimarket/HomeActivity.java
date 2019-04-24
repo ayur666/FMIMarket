980739cd08f09e5126c5bcfb617c8b4563a6413f
= <ALEXANDRU.TAPIRDEA@my.fmi.unibuc.ro>
2019-04-25 01:42:44 +0300

    Added the cart functionality with display,edit and delete for the app

--- app/src/main/java/com/example/fmimarket/HomeActivity.java
+++ app/src/main/java/com/example/fmimarket/HomeActivity.java
@@ -65,8 +65,8 @@ public class HomeActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(HomeActivity.this, CartActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -186,7 +186,8 @@ public class HomeActivity extends AppCompatActivity
 
         if (id == R.id.nav_cart)
         {
-
+            Intent intent = new Intent(HomeActivity.this, CartActivity.class);
+            startActivity(intent);
         }
         else if (id == R.id.nav_orders)
         {
