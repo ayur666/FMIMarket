48a5ecc0858e006d73a9943ccc0746b255dc796b
eleni tzellas <lex.cojocaru@gmail.com>
2019-04-24 21:18:55 +0300

    First Iteration

---
+++ app/src/main/java/com/example/fmimarket/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.example.fmimarket.Prevalent;
+
+import com.example.fmimarket.Model.Users;
+
+public class Prevalent
+{
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
